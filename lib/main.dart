2749f59b6180eb3f97105eb6b627168d0ea258bd
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2023-09-16 00:48:11 -0400

    relocated pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:remedi/loginScreen.dart';
+import 'package:remedi/pages/loginScreen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:remedi/data-models/schema.dart';
 import 'package:remedi/pages/dashboard.dart';
